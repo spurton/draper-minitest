086ae9250c1982be2133d9e7e8d984042a6be8e1
Scott Burton <spurton@gmail.com>
2012-05-21 15:16:08 -0700

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DraperMinitest::Application.config.secret_token = 'e04334851e81d50c4551ad71954b77423ed4bbf0e62331e3d180747836bf18be732a51369376c7b3ad0a61ec445636f9d41dbd06da30f552a1488752ce978231'
